b36ef91e8714a9b5daec0c259027122b5192e0d2
ses10 <sesma@live.com>
2016-03-25 10:40:05 -0400

    add deviant_scraper

---
+++ deviant_scraper.py
@@ -0,0 +1,18 @@
+'''
+deviant_scraper.py
+
+Scrapes the images from a DeviantArt search page.
+
+'''
+from lxml import html
+import requests
+
+#what image to search for, spaces have to be replaced by "+" in query
+query = 'adventure+time'
+queryString = 'http://www.deviantart.com/browse/all/?section=&global=1&q=' + query
+#the current xPath for image results as of writing
+xPath = '//*[@id="page-1-results"]/div/span/span[1]/span[2]/a/img'
+
+
+
+
