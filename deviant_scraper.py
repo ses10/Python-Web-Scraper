31bbf50520092268538ba2df10f3eaf24fdd8ab4
ses10 <sesma@live.com>
2016-03-25 11:52:30 -0400

    output scraped images into html

--- deviant_scraper.py
+++ deviant_scraper.py
@@ -3,6 +3,7 @@ deviant_scraper.py
 
 Scrapes the images from a DeviantArt search page.
 
+So far only scrapes from 1st results page
 '''
 from lxml import html
 import requests
@@ -19,3 +20,26 @@ tree = html.fromstring(page.content)
 
 #get the images
 images = tree.xpath(xPath)
+
+#output images to html file
+f = open("deviantart-results.html", 'w')
+
+f.write('''
+    <!Doctype HTML5>
+    <html>
+        <body>
+            <ul>
+''')
+
+
+#write each image url
+for image in images:
+    f.write('<li><img src="' + image.get('src') + '" ></li>' )
+
+f.write('''
+            </ul>
+        </body>
+    </html>
+''')
+
+f.close()
